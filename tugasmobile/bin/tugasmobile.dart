62f0bcc229c9a8e1560a4d42408bd682ee1b0cb0
MuhammadSalwan999 <salwanxd999@contoh.com>
2025-08-22 18:32:56 +0900

    first commit

---
+++ tugasmobile/bin/tugasmobile.dart
@@ -0,0 +1,5 @@
+import 'package:tugasmobile/tugasmobile.dart' as tugasmobile;
+
+void main(List<String> arguments) {
+  print('Hello world: ${tugasmobile.calculate()}!');
+}
---
+++ tugasmobile/test/tugasmobile_test.dart
@@ -0,0 +1,8 @@
+import 'package:tugasmobile/tugasmobile.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
